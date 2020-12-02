cf2d752c89ccd065106318268ff18e87506a0380
Samal <samal.zhex@gmail.com>
2020-12-02 12:26:54 +0600

    добавила в search controller метод поиска по get запросу

---
+++ searchApp/Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace searchApp.Controllers
+{ 
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Search> Get()
+        {
+            return new[] {
+                new Search { Id = 1, Name = "Chop Suey"},
+                new Search { Id = 2, Name = "Toxicity" }
+            };
+        }
+
+        [HttpGet("{query}")]
+        public IActionResult Get(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return BadRequest();
+            }
+
+            var items = Get();
+            var result = items.Any(i => i.Name.ToLower() == query.ToLower());
+
+            if(result)
+            {
+                return Ok(query);
+            }
+            return NotFound();
+
+        }
+    }
+}
---
+++ searchApp/Search.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace searchApp
+{
+    public class Search
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+    }
+}
